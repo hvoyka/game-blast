02452e7d1011fa66796bf0d10a4eac3a98574ae2
Stanislav Lavrentev <hvoinii@gmail.com>
2023-08-28 22:47:34 +0600

    feat: projectile and enemy collision

--- scr/main.js
+++ scr/main.js
@@ -3,6 +3,7 @@ import "./assets/style.css";
 import { config } from "./config.js";
 import { Player } from "./classes/Player";
 import { Projectile } from "./classes/Projectile";
+import { Enemy } from "./classes/Enemy";
 
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
@@ -14,15 +15,60 @@ const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 
 const player = new Player(centerX, centerY, 30, "blue");
-
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4;
+    let x, y;
+    if (Math.random() > 0.5) {
+      x = Math.random() > 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() > 0.5 ? 0 - radius : canvas.height + radius;
+    }
+
+    const color = "green";
+
+    const angle = Math.atan2(centerY - y, centerX - x);
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+    console.log(enemies);
+  }, [1000]);
+}
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.draw(c);
+
   projectiles.forEach((projectile) => {
     projectile.update(c);
+  });
+
+  enemies.forEach((enemy, enemyIndex) => {
+    enemy.update(c);
+
+    projectiles.forEach((projectile, projectileIndex) => {
+      const distance = Math.hypot(
+        projectile.x - enemy.x,
+        projectile.y - enemy.y
+      );
+      if (distance - enemy.radius - projectile.radius < 1) {
+        console.log("remove", distance);
+        setTimeout(() => {
+          enemies.splice(enemyIndex, 1);
+          projectiles.splice(projectileIndex, 1);
+        }, 0);
+      }
+    });
   });
 }
 
@@ -38,3 +84,4 @@ addEventListener("click", (event) => {
 });
 
 animate();
+spawnEnemies();
